db16c347c5589c1c8e9ea9d6c9ee8d24f6c896c5
arbonboy <arbonboy@gmail.com>
2015-09-21 14:20:23 -0600

    Update serviceNowBuild.py
    Fixing the quotes character on the authMatch line I added...darn copy and paste

--- serviceNowBuild.py
+++ serviceNowBuild.py
@@ -3,7 +3,7 @@ Copyright (c) 2013 Fruition Partners, Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
+in the Software without restriction, including without limitation the rightsau
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
@@ -234,7 +234,7 @@ def get_authentication(sublimeClass, edit):
     reg = sublime.Region(0, sublimeClass.view.size())
     text = sublimeClass.view.substr(reg)
 
-    authMatch = re.search(r”__authentication[\W=]*([a-zA-Z0-9:~`\/\!@#$%\^&*()_\-\\;,.]*)”, text)
+    authMatch = re.search(r"__authentication[\W=]*([a-zA-Z0-9:~`\/\!@#$%\^&*()_\-\\;,.]*)", text)
 
     if authMatch and authMatch.groups()[0] != "STORED":
         user_pass = authMatch.groups()[0]
